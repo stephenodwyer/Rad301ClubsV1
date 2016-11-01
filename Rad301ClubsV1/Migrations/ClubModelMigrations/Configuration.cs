dd0706062be676c481c1a5135c3febe234e6456a
ITSPaul <powell.paul@itsligo.ie>
2016-11-01 18:52:53 +0000

    added seeding of Student Data using CVSHelper and embedded reference

--- Rad301ClubsV1/Migrations/ClubModelMigrations/Configuration.cs
+++ Rad301ClubsV1/Migrations/ClubModelMigrations/Configuration.cs
@@ -1,10 +1,14 @@
 namespace Rad301ClubsV1.Migrations.ClubModelMigrations
 {
+    using CsvHelper;
     using Models.ClubModel;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.IO;
     using System.Linq;
+    using System.Reflection;
+    using System.Text;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Rad301ClubsV1.Models.ClubModel.ClubContext>
     {
@@ -20,6 +24,27 @@ namespace Rad301ClubsV1.Migrations.ClubModelMigrations
                 new Club { ClubName = "The Tiddly Winks Club", CreationDate = DateTime.Now });
             context.Clubs.AddOrUpdate(c => c.ClubName,
                 new Club { ClubName = "The Chess Club", CreationDate = DateTime.Now });
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "Rad301ClubsV1.Migrations.ClubModelMigrations.TestStudents.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.HasHeaderRecord = false;
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    var testStudents = csvReader.GetRecords<Student>().ToArray();
+                    context.Students.AddOrUpdate(s => s.StudentID, testStudents);
+                }
+            }
+            //try
+            //{
+            //    StudentData testStudents = new StudentData();
+            //}
+            //catch(Exception e) {
+            //    throw new Exception { Source = e.Message };
+            //}
         }
     }
 }
---
+++ Rad301ClubsV1/Migrations/ClubModelMigrations/StudentData.cs
@@ -0,0 +1,44 @@
+﻿using Rad301ClubsV1.Models.ClubModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Rad301ClubsV1.Migrations.ClubModelMigrations
+{
+    public class StudentData
+    {
+
+        public List<Student> studentSeeds = new List<Student>();
+
+        public StudentData()
+        {
+            if (File.Exists(@".\Test Students.csv"))
+                studentSeeds = File.ReadAllLines(@".\Test Students.csv")
+                                               .Select(v => FromCsv(v)).ToList();
+            else throw new 
+                    Exception {
+                Source = "Student Data class" + @".\Test Students.csv" + " does not exist",
+                 
+                 
+                 };
+        }
+
+        public static Student FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            Student StudentTestData = new Student();
+            StudentTestData.StudentID = values[0];
+            StudentTestData.Fname = values[1];
+            StudentTestData.Sname = values[2];
+            //dailyValues.playerid = Guid.NewGuid();
+            //dailyValues.FirstName = values[0];
+            //dailyValues.SecondName = values[1];
+            //dailyValues.GamerTag = values[2];
+            //dailyValues.topscore = Int32.Parse(values[3]);
+
+            return StudentTestData;
+        }
+    }
+}
